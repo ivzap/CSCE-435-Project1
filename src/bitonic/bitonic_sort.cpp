175410265dc54fc32a65a8659e5003c7fd9b393b
ethanrutt <ethan.rutt2032@gmail.com>
2024-10-14 17:48:04 -0500

    add command line parsing

--- src/bitonic/bitonic_sort.cpp
+++ src/bitonic/bitonic_sort.cpp
@@ -4,6 +4,8 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <vector>
+#include <stdlib.h>
+#include <getopt.h>
 
 #include <adiak.hpp>
 #include <caliper/cali-manager.h>
@@ -63,46 +65,80 @@ void bitonic_sort_p(std::vector<int> arr, int total_processes, int rank);
 
 int main(int argc, char *argv[]) {
 /******************************************************************************
+ * Start Command Line Option Parsing
+ ******************************************************************************/
+char* input_type = nullptr;
+int num_elements = -1;
+int num_procs = -1;
+int opt;
+
+while ((opt = getopt(argc, argv, "t:n:p:")) != -1) {
+	switch(opt) {
+		case 't':
+			input_type = optarg;
+			break;
+		case 'n':
+			num_elements = atoi(optarg);
+			break;
+		case 'p':
+			num_procs = atoi(optarg);
+			break;
+		default:
+			std::cout << "Unknown option received, exiting" << std::endl;
+			return 1;
+	}
+}
+
+if (input_type == nullptr || num_elements == -1 || num_procs == -1) {
+	std::cout << "Command line arguments are needed for this program to function. Please indicate the input type: {random, sorted, reverse, perturbed}, the number of elements: {int}, and the number of processes {int}." << std::endl;
+	return 1;
+}
+/******************************************************************************
+ * End Command Line Option Parsing
+ ******************************************************************************/
+/******************************************************************************
  * Start Adiak Metadata
  ******************************************************************************/
-  const char* algorithm = "bitonic sort";
-  const char* programming_model = "mpi";
-  const char* data_type = "int";
-  const char* size_of_data_type = "4";
-  const char* input_size = "4";
-  const char* input_type = "random";
-  const char* num_procs = "4";
-  const char* scalability = "strong";
-  const char* group_number = "6";
-  const char* implementation_source = "handwritten";
+  char* adiak_algorithm = "bitonic sort";
+  char* adiak_programming_model = "mpi";
+  char* adiak_data_type = "int";
+  char* adiak_size_of_data_type = "4";
+  char* adiak_input_type = input_type;
+  char* adiak_input_size;
+  sprintf(adiak_input_size, "%d", num_elements);
+  char* adiak_num_procs;
+  sprintf(adiak_num_procs, "%d", num_procs);
+  char* adiak_scalability = "strong";
+  char* adiak_group_number = "6";
+  char* adiak_implementation_source = "handwritten";
 
   adiak::init(NULL);
   adiak::launchdate();                  // launch date of the job
   adiak::libraries();                   // Libraries used
   adiak::cmdline();                     // Command line used to launch the job
   adiak::clustername();                 // Name of the cluster
-  adiak::value("algorithm", algorithm); // The name of the algorithm you are
+  adiak::value("algorithm", adiak_algorithm); // The name of the algorithm you are
                                         // using (e.g., "merge", "bitonic")
-  adiak::value("programming_model", programming_model); // e.g. "mpi"
+  adiak::value("programming_model", adiak_programming_model); // e.g. "mpi"
   adiak::value(
       "data_type",
-      data_type); // The datatype of input elements (e.g., double, int, float)
+      adiak_data_type); // The datatype of input elements (e.g., double, int, float)
   adiak::value("size_of_data_type",
-               size_of_data_type); // sizeof(datatype) of input elements in
+               adiak_size_of_data_type); // sizeof(datatype) of input elements in
                                    // bytes (e.g., 1, 2, 4)
   adiak::value("input_size",
-               input_size); // The number of elements in input dataset (1000)
+               adiak_input_size); // The number of elements in input dataset (1000)
   adiak::value("input_type",
-               input_type); // For sorting, this would be choices: ("Sorted",
+               adiak_input_type); // For sorting, this would be choices: ("Sorted",
                             // "ReverseSorted", "Random", "1_perc_perturbed")
-  adiak::value("num_procs", num_procs); // The number of processors (MPI ranks)
+  adiak::value("num_procs", adiak_num_procs); // The number of processors (MPI ranks)
   adiak::value("scalability",
-               scalability); // The scalability of your algorithm. choices:
+               adiak_scalability); // The scalability of your algorithm. choices:
                              // ("strong", "weak")
   adiak::value("group_num",
-               group_number); // The number of your group (integer, e.g., 1, 10)
+               adiak_group_number); // The number of your group (integer, e.g., 1, 10)
   adiak::value("implementation_source",
-               implementation_source); // Where you got the source code of your
+               adiak_implementation_source); // Where you got the source code of your
                                        // algorithm. choices: ("online", "ai",
                                        // "handwritten").
 /******************************************************************************
@@ -112,9 +148,7 @@ int main(int argc, char *argv[]) {
 /******************************************************************************
  * Start MPI Initialization
  ******************************************************************************/
-  int numtasks, taskid, source, dest, mtype;
-
-  MPI_Init(&argc, &argv);
+return 0;
 }
 
 /******************************************************************************
@@ -174,9 +208,9 @@ void bitonic_sort_p(std::vector<int> arr, int total_processes, int rank) {
     for (int j = i - 1; j > 0; j--) {
       if (((window_id % 2 == 0) && (rank >> j == 0)) ||
           (window_id % 2 == 1) && (rank >> j == 1)) {
-        compare(j, arr, 0, count, 0);
+        compare(j, arr, 0, arr.size(), 0);
       } else {
-        compare(j, arr, 0, count, 1);
+        compare(j, arr, 0, arr.size(), 1);
       }
     }
   }
