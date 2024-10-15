f39613e221a6246cf53026ef95c339ea5ae534a9
ethanrutt <ethan.rutt2032@gmail.com>
2024-10-14 19:02:20 -0500

    initial program complete

--- src/bitonic/bitonic_sort.cpp
+++ src/bitonic/bitonic_sort.cpp
@@ -14,6 +14,15 @@
 /******************************************************************************
  * Start Helper Function Headers
  ******************************************************************************/
+/**
+ * checks if the local arrays are sorted
+ *
+ * @param arr_chunk the local chunk of the array
+ * @param arr_size the size of the array
+ * @param rank the rank of the calling process
+ * @param p the amount of processes
+ */
+bool is_sorted_p(int* arr_chunk, int arr_size, int rank, int p);
 
 /**
  * get_most_significant_n_bits() gets the most significant n bits starting at
@@ -35,29 +44,32 @@ int get_most_significant_n_bits(int num, int num_of_bits);
  * @param direction the direction to merge (increasing or decreasing). 1 for
  * increasing 0 for decreasing
  */
-void bitonic_merge(std::vector<int> arr, int low, int count, bool direction);
+void bitonic_merge(int arr[], int low, int count, bool direction);
 
 /**
  * compare() is a parallel merging function for bitonic sort
  *
  * @param rank_to_send_to the processes rank to MPI_Sendrecv to
+ * @param rank the processes rank that is calling the function
  * @param arr the array to be sorted
+ * @param num_elements number of elements in the array
  * @param low the low index to start merging from
  * @param count the number of elements to merge
  * @param direction the direction to merge (increasing or decreasing). 1 for
  * increasing 0 for decreasing
  */
-void compare(int rank_to_send_to, std::vector<int> arr, int low, int count,
+void compare(int rank_to_send_to, int rank, int arr[], int num_elements, int low, int count,
              bool direction);
 
 /**
  * bitonic_sort_p() is a parallel sorting algorithm that implements bitonic sort
  *
  * @param arr the array to be sorted
+ * @param num_elements the number of elements in the array
  * @param total_processes the total number of processes
  * @param rank this processes rank in the MPI_COMM_WORLD
  */
-void bitonic_sort_p(std::vector<int> arr, int total_processes, int rank);
+void bitonic_sort_p(int arr[], int num_elements, int total_processes, int rank);
 
 /******************************************************************************
  * End Helper Function Headers
@@ -90,7 +102,7 @@ while ((opt = getopt(argc, argv, "t:n:p:")) != -1) {
 }
 
 if (input_type == nullptr || num_elements == -1 || num_procs == -1) {
-	std::cout << "Command line arguments are needed for this program to function. Please indicate the input type: {random, sorted, reverse, perturbed}, the number of elements: {int}, and the number of processes {int}." << std::endl;
+	std::cerr << "Command line arguments are needed for this program to function. Please indicate the input type: {random, sorted, reverse, perturbed}, the number of elements: {int}, and the number of processes {int}." << std::endl;
 	return 1;
 }
 /******************************************************************************
@@ -148,12 +160,87 @@ if (input_type == nullptr || num_elements == -1 || num_procs == -1) {
 /******************************************************************************
  * Start MPI Initialization
  ******************************************************************************/
+int rank, num_tasks;
+MPI_Init(&argc, &argv);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
+
+if (num_procs != num_tasks) {
+	std::cerr << "num procs passed in as a cmdline arg doesn't equal MPI tasks" << std::endl;
+	return 1;
+}
+/******************************************************************************
+ * End MPI Initialization
+ ******************************************************************************/
+
+/******************************************************************************
+ * Start Data Generation
+ ******************************************************************************/
+int elements_per_proc = num_elements / num_tasks;
+
+int input[num_elements];
+
+for (int i = 0; i < num_elements; i++) {
+	input[i] = rand() % 2000;
+}
+
+int local[num_elements];
+MPI_Scatter(input, elements_per_proc, MPI_INT, local, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+
+/******************************************************************************
+ * End Data Generation
+ ******************************************************************************/
+/******************************************************************************
+ * Start Bitonic Sort
+ ******************************************************************************/
+bitonic_sort_p(local, elements_per_proc, num_tasks, rank);
+/******************************************************************************
+ * End Bitonic Sort
+ ******************************************************************************/
+/******************************************************************************
+ * Start Correctness Check
+ ******************************************************************************/
+MPI_Barrier(MPI_COMM_WORLD);
+bool sorted = is_sorted_p(local, num_elements, rank, num_tasks);
+if (rank == 0 && sorted) {
+	std::cout << "SUCCESS" << std::endl
+}
+else if (rank == 0 && !sorted) {
+	std::cerr << "Failed sorting" << std::endl;
+	return 1;
+}
+/******************************************************************************
+ * End Correctness Check
+ ******************************************************************************/
+MPI_Barrier(MPI_COMM_WORLD);
+MPI_Finalize();
 return 0;
 }
 
 /******************************************************************************
  * Start Helper Function Definitions
  ******************************************************************************/
+bool is_sorted_p(int* arr_chunk, int arr_size, int rank, int p){
+    // verify each local arr_chunk is sorted then the border or p+1 first element
+    for (int i = 0; i < arr_size - 1; i++) {
+        if (arr_chunk[i] > arr_chunk[i + 1]) {
+            return 0;
+        }
+    }
+
+    int last_elements[p];
+    MPI_Gather(&arr_chunk[arr_size - 1], 1, MPI_INT, last_elements, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        // check the last element of each segment to ensure the overall order
+        for (int i = 0; i < p - 1; i++) {
+            if (last_elements[i] > last_elements[i + 1]) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    return 0;
+}
 
 int get_most_significant_n_bits(int num, int num_of_bits) {
   if (num == 0) {
@@ -173,7 +260,7 @@ int get_most_significant_n_bits(int num, int num_of_bits) {
   return result;
 }
 
-void bitonic_merge(std::vector<int> arr, int low, int count, bool direction) {
+void bitonic_merge(int arr[], int low, int count, bool direction) {
   int k;
   if (count <= 1) {
     return;
@@ -191,15 +278,17 @@ void bitonic_merge(std::vector<int> arr, int low, int count, bool direction) {
   bitonic_merge(arr, low + k, k, direction);
 }
 
-void compare(int rank_to_send_to, std::vector<int> arr, int low, int count,
+void compare(int rank_to_send_to, int rank, int arr[], int num_elements, int low, int count,
              bool direction) {
-  // MPI_Sendrev(rank_to_send_to, ...);
+  int send[num_elements];
+  int recv[num_elements];
+  MPI_Sendrecv(send, num_elements, MPI_INT, rank_to_send_to, 0, recv, num_elements, MPI_INT, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   bitonic_merge(arr, low, count, direction);
 }
 
-void bitonic_sort_p(std::vector<int> arr, int total_processes, int rank) {
+void bitonic_sort_p(int arr[], int num_elements, int total_processes, int rank) {
   int dimension = static_cast<int>(log2(total_processes));
-  std::sort(arr.begin(), arr.end());
+  std::sort(arr[0], arr[num_elements]);
 
   for (int i = 1; i < dimension; i++) {
     int num_bits = dimension - i;
@@ -208,9 +297,9 @@ void bitonic_sort_p(std::vector<int> arr, int total_processes, int rank) {
     for (int j = i - 1; j > 0; j--) {
       if (((window_id % 2 == 0) && (rank >> j == 0)) ||
           (window_id % 2 == 1) && (rank >> j == 1)) {
-        compare(j, arr, 0, arr.size(), 0);
+        compare(j, arr, 0, num_elements, 0);
       } else {
-        compare(j, arr, 0, arr.size(), 1);
+        compare(j, arr, 0, num_elements, 1);
       }
     }
   }
